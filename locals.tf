b97a014b36071f6d8d582804c1a06ac90199b225
Maciej Rachuna <rachuna.maciej@gmail.com>
2025-06-15 12:47:21 +0200

    feat: DEVOPS-22 Optymalizacja modułu gitla-group

--- locals.tf
+++ locals.tf
@@ -1,16 +1,3 @@
 locals {
-  allowed_icon_types = [
-    "",
-    "ansible",
-    "containers",
-    "dev-tools",
-    "gitlab",
-    "infrastructure",
-    "mikrotik",
-    "packer",
-    "proxmox",
-    "root",
-    "semantic-release",
-    "terraform",
-  ]
+  allowed_icon_types = jsondecode(file("${path.module}/data/allowed_icon_types.json"))
 }
